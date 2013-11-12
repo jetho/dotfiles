80b96b311c101fd120577dfb99fc2b356f1abf26
jetho <jetho@gmx.de>
2013-11-12 19:33:43 +0100

    Updated config files

--- dotfiles/.config/dwb/userscripts/defer-loading.js
+++ dotfiles/.config/dwb/userscripts/defer-loading.js
@@ -1,10 +1,9 @@
 //!javascript
 if (settings.loadOnFocus === false) {
     execute('local_set load-on-focus true');
+}
+Signal.connect('navigation', function(webview) {
+    if (webview == tabs.current) {
+        execute('local_set load-on-focus false');
+        this.disconnect();
     }
-    Signal.connect('navigation', function(webview) {
-        if (webview == tabs.current) {
-                execute('local_set load-on-focus false');
-                        this.disconnect();
-                            }
-                            });
