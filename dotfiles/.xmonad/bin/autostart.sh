0ac8af4cf77b0ed10e75f8188270f27e986c9c40
Jens Thomas <jens@debian.lan>
2014-02-18 12:31:16 +0100

    Updated tmux config

--- dotfiles/.xmonad/bin/autostart.sh
+++ dotfiles/.xmonad/bin/autostart.sh
@@ -8,7 +8,7 @@ sh ~/.fehbg &
 trayer --edge bottom --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --widthtype percent --transparent true --tint black --alpha 0 --height 20 &
 
 # Programs which will run after Xmonad has started
-urxvtc +bc +uc -cr Green -name tmux -e tmux new -s basic &
+urxvtc +bc +uc -cr Green -name tmux -e tmux attach &
 firefox &
 urxvtc -name weechat -e weechat-curses &
 urxvtc -name ncmpcpp -e ncmpcpp &
