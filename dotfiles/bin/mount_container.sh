7df6b25a0fa02e7a313b5b36a867a3ac28d3bed9
jetho <jetho@gmx.de>
2014-06-28 17:26:32 -0300

    Update encryption scripts

--- dotfiles/bin/mount_container.sh
+++ dotfiles/bin/mount_container.sh
@@ -2,6 +2,8 @@
 
 ## see http://www.smallbusinesstech.net/more-complicated-instructions/encyrption/luks-encyrption
 
-sudo losetup /dev/loop0 /path/to/container
-sudo cryptsetup luksOpen /dev/loop0 encr-container
+LOOPDEV=$(sudo losetup -f)
+
+sudo losetup $LOOPDEV /path/to/container
+sudo cryptsetup luksOpen $LOOPDEV encr-container
 mount /dev/mapper/encr-container
--- dotfiles/bin/umount_container.sh
+++ dotfiles/bin/umount_container.sh
@@ -2,6 +2,8 @@
 
 ## see http://www.smallbusinesstech.net/more-complicated-instructions/encyrption/luks-encyrption
 
+LOOPDEV=$(sudo losetup -a | grep "/container" | cut -d: -f 1)
+
 umount /dev/mapper/encr-container
 sudo cryptsetup luksClose encr-container
-sudo losetup -d /dev/loop0
+sudo losetup -d $LOOPDEV
