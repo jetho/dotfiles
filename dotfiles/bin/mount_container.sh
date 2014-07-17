2739cadf3da0c6a5995045f3735bb266275a5c08
Jens Thomas <jetho@gmx.de>
2014-07-17 20:44:43 -0300

    Update mount_container.sh

--- dotfiles/bin/mount_container.sh
+++ dotfiles/bin/mount_container.sh
@@ -2,6 +2,13 @@
 
 ## see http://www.smallbusinesstech.net/more-complicated-instructions/encyrption/luks-encyrption
 
+IDENTIFIER="/container"
+
+if [ "`sudo losetup -a | grep -c $IDENTIFIER`" != "0" ]; then 
+    echo "Already mounted!"
+    exit
+fi
+
 LOOPDEV=$(sudo losetup -f)
 
 sudo losetup $LOOPDEV /path/to/container
