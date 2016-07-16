0f078aa6a377c4b5be03451599995115b3176acc
jetho <jetho@gmx.de>
2016-07-16 14:40:49 -0300

    switch from roxterm to xfce4-terminal

--- xmonad/.xmonad/bin/autostart.sh
+++ xmonad/.xmonad/bin/autostart.sh
@@ -10,18 +10,18 @@ sh ~/.fehbg &
 trayer --edge bottom --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --widthtype percent --transparent true --tint black --alpha 0 --height 20 &
 
 # Programs which will run after Xmonad has started
-roxterm --title=tmux -e "tmux -2 attach" &
+xfce4-terminal --title=tmux -e "tmux -2 attach" &
 sleep 3
-roxterm --title=ranger -e ranger &
+xfce4-terminal --title=ranger -e ranger &
 firefox &
-roxterm --title=weechat -e weechat-curses &
-roxterm --title=ncmpcpp -e ncmpcpp &
-roxterm --title=mutt -e mutt &
+xfce4-terminal --title=weechat -e weechat-curses &
+xfce4-terminal --title=ncmpcpp -e ncmpcpp &
+xfce4-terminal --title=mutt -e mutt &
 sleep 2 
-roxterm --title=newsbeuter -e newsbeuter & 
-roxterm --title=sysmail -e "mutt -f /var/mail/$USER -e 'set timeout=10'" & 
+xfce4-terminal --title=newsbeuter -e newsbeuter & 
+xfce4-terminal --title=sysmail -e "mutt -f /var/mail/$USER -e 'set timeout=10'" & 
 sleep 2 
-roxterm --title=log -e "sudo tail -f -n 50 /var/log/messages" &
+xfce4-terminal --title=log -e "sudo tail -f -n 50 /var/log/messages" &
 sleep 2
-roxterm --title=htop -e htop &
+xfce4-terminal --title=htop -e htop &
 
